dc26d9cecf9c0ce98258b3fea48767488fa5f4ee
tovantai <tovantaidz2001@gmail.com>
2022-08-11 15:52:15 +0700

    install mui, apdate pages/ productList

--- src/pages/productList/ProductItem.jsx
+++ src/pages/productList/ProductItem.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import {baseUrlImg} from "../../configs/configs"
 import {product1, saleIcon} from "../../access/data/data"
 import {numberWithComas, formatString} from "../../utils/utils"
-const ProductItem = (props) => {
+const ProductItem = ({data}) => {
     return (
         <div className="col col-xs-6 col-sm-6 col-md-6 col-lg-3">
-            <div className="productList__item">{/* discount */}
-            <Link to={"/product-detail/1"} className="productList__item__link">
+            <div className={Number(data.discount)!==0?"productList__item discount":"productList__item"}>{/* discount */}
+            <Link to={"/product-detail/"+data.id} className="productList__item__link">
                 <div className="productList__item__link-img">
-                    <img src={product1} alt="" />
+                    <img src={`${baseUrlImg}${data.background}`} alt={data.model} />
                 </div>
                 <div className="productList__item__link__about">
                     <p className="productList__item__link__about-model">
-                        Laptop Gaming Dell G15 5511
+                        {formatString(data.model)}
                     </p>
                     <p className="productList__item__link__about-screen">
-                        15.6\" FHD (1920 x1080) 120Hz, 250 nits, WVA, Anti-Glare, LED Backlit, Narrow Border Display
+                        {formatString(data.screen)}
                     </p>
                     <p className="productList__item__link__about-cpu">
-                    Intel Core i5-11400H 2.7GHz up to 4.5GHz 12MB
+                        {formatString(data.CPU)}
                     </p>
                     <p className="productList__item__link__about-ram">
-                    8GB (8x1) DDR4 3200MHz (2x SO-DIMM socket, up to 32GB SDRAM)
+                        {formatString(data.RAM)}
                     </p>
                 </div>
                 <div className="productList__item__link-link">
@@ -31,15 +32,13 @@ const ProductItem = (props) => {
             </Link>
             <div className="productList__item__about">
                 <p className="productList__item__about-name">
-                    Laptop MSI Modern 14 B11MOU 1028VN Laptop MSI Modern 14
-                    B11MOU 1028VN Laptop MSI Modern 14 B11MOU 1028VN Laptop MSI
-                    Modern 14 B11MOU 1028VN
+                    {formatString(data.model)}
                 </p>
                 <p className="productList__item__about__price-old">
-                    14,490,000<u>đ</u>
+                    {Number(data.discount)!==0?numberWithComas(data.price):""}<u>đ</u>
                 </p>
                 <p className="productList__item__about__price-new">
-                    14,490,000<u>đ</u>
+                    {Number(data.discount)!==0? numberWithComas(Number(data.price)*(100 - Number(data.discount))/100):numberWithComas(data.price)}<u>đ</u>
                 </p>
                 <span className="productList__item__about__discount">
                     <img
@@ -48,7 +47,7 @@ const ProductItem = (props) => {
                         alt=""
                     />
                     <span className="productList__item__about__discount-percent">
-                        -24%
+                        -{Number(data.discount)!==0?data.discount:"0"}%
                     </span>
                 </span>
             </div>
--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -1,85 +1,118 @@
 import React, { useState, useEffect } from "react";
-import {useSearchParams} from "react-router-dom"
+import { useSearchParams } from "react-router-dom";
 import { baseUrlApi, baseUrlImg } from "../../configs/configs";
 import ProductItem from "./ProductItem";
+import Pagination from "@mui/material/Pagination";
 const ProductList = () => {
-    
-    let generateUrlParams = (params)=>{
-        let result=""
-        let page, limit
-        page = params.page ? params.page : "1"
-        limit = params.limit ? params.limit : "8"
-        if(!params['category-name']&&!params['search']){
-            result += `?page=${page}&limit=${limit}`
-        }else if(params['category-name']){
-            result += `?category-name=${params["category-name"]}&page=${page}&limit=${limit}`
-        }else if(params['search']){
-            result += `?search=${params.search}&page=${page}&limit=${limit}`
+    let generateUrlParams = (params) => {
+        let result = "";
+        let page, limit;
+        page = params.page ? params.page : "1";
+        limit = params.limit ? params.limit : "8";
+        if (!params["category-name"] && !params["search"]) {
+            result += `?page=${page}&limit=${limit}`;
+        } else if (params["category-name"]) {
+            result += `?category-name=${params["category-name"]}&page=${page}&limit=${limit}`;
+        } else if (params["search"]) {
+            result += `?search=${params.search}&page=${page}&limit=${limit}`;
         }
-        return result
-    }
-    let generateParams = (params)=>{
-        let result = {}
-        let page, limit
-        page = params.page ? params.page : "1"
-        limit = params.limit ? params.limit : "8"
-        if(page!=="1"){
-            result.page=page
+        return result;
+    };
+    let generateParams = (params) => {
+        let result = {};
+        let page, limit;
+        page = params.page ? params.page : "1";
+        limit = params.limit ? params.limit : "8";
+        if (page !== "1") {
+            result.page = page;
         }
-        if(limit!=="8"){
-            result.limit=limit
+        if (limit !== "8") {
+            result.limit = limit;
         }
-        if(!params['category-name']&&!params['search']){
-            return result
+        if (!params["category-name"] && !params["search"]) {
+            return result;
             // result = {"page":page,"category-name":null,"search":null, "limit":limit}
-        }else if(params['category-name']){
-            result["category-name"] = params['category-name']
-            return result
+        } else if (params["category-name"]) {
+            result["category-name"] = params["category-name"];
+            return result;
             // result = {"page":page,"category-name":params['category-name'],"search":null, "limit":limit}
-        }else if(params['search']){
-            result.search = params.search
+        } else if (params["search"]) {
+            result.search = params.search;
             // result = {"page":page,"category-name":null,"search":params['search'], "limit":limit}
         }
-        return result
-    }
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [productsData, setProductsData] = useState({})
-    let params = Object.fromEntries(searchParams.entries())
-    let [page, setPage]= useState(params.page)
-    let [limit, setLimit]= useState(params.limit)
-    let [categoryName,setCategoryName] = useState(params['category-name'])
-    let [searchName,setSearchName] = useState(params['search'])
-    useEffect(()=>{
-        if(page!==params.page){
-            setPage(params.page)
+        return result;
+    };
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [productsData, setProductsData] = useState({});
+    let params = Object.fromEntries(searchParams.entries());
+    let [page, setPage] = useState(params.page);
+    let [limit, setLimit] = useState(params.limit);
+    let [categoryName, setCategoryName] = useState(params["category-name"]);
+    let [searchName, setSearchName] = useState(params["search"]);
+    useEffect(() => {
+        if (page !== params.page) {
+            setPage(params.page);
         }
-        if(limit!==params.limit){
-            setLimit(params.limit)
+        if (limit !== params.limit) {
+            setLimit(params.limit);
         }
-        if(categoryName!==params['category-name']){
-            setCategoryName(params['category-name'])
+        if (categoryName !== params["category-name"]) {
+            setCategoryName(params["category-name"]);
         }
-        if(searchName!==params['search']){
-            setSearchName(params['search'])
+        if (searchName !== params["search"]) {
+            setSearchName(params["search"]);
         }
-    })
-    useEffect(()=>{
-        fetch(`${baseUrlApi}products.php${generateUrlParams(params)}`,{
+    });
+    useEffect(() => {
+        fetch(`${baseUrlApi}products.php${generateUrlParams(params)}`, {
             method: "GET",
-            credentials: "include"
-        }).then(res=>{
-            if(res.status===200||res.status===201){
-                res.json().then(res=>{
-                    setProductsData(res)
-                })
+            credentials: "include",
+        }).then((res) => {
+            if (res.status === 200 || res.status === 201) {
+                res.json().then((res) => {
+                    setProductsData(res);
+                });
             }
-        })
-        console.log("re-render");
-    },[page, limit,categoryName,searchName])
+        });
+    }, [page, limit, categoryName, searchName]);
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+    const handleChangePagination = (event, value) => {
+        if(categoryName||searchName){
+            window.scrollTo(0,0)
+        }else{
+            if(document.documentElement.clientWidth<=768){
+                window.scrollTo(0,(document.documentElement.clientWidth/2)+78)
+            }else{
+                window.scrollTo(0,((document.documentElement.clientWidth - 270) / 2))
+            }
+        }
+        
+        params.page = value+""
+        setSearchParams(generateParams(params));
+    };
     return (
-        <div className="productList__container row">
-            <ProductItem />
-        </div>
+        <>
+            <div className="productList__container row">
+                {/* <ProductItem /> */}
+                {productsData.data &&
+                    productsData.data.map((product) => (
+                        <ProductItem key={product.id} data={product} />
+                    ))}
+            </div>
+            <div className="productList__pagination">
+                {productsData.data && Number(productsData.pagination.totalPage)>1 && (
+                    <Pagination
+                        count={Number(productsData.pagination.totalPage)}
+                        color="primary"
+                        size="large"
+                        page={Number(productsData.pagination.currentPage)}
+                        onChange={handleChangePagination}
+                    />
+                )}
+            </div>
+        </>
     );
 };
 
